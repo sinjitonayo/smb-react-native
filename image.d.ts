ad7c57497222335509e897ac427cbf8007117324
sinjitonayo <tanakashinji23@gmail.com>
2025-02-26 21:49:37 +0800

    feat: Update app configuration with new font assets and colors, and add image type declarations

---
+++ image.d.ts
@@ -0,0 +1,36 @@
+declare module "*.png" {
+  const value: any;
+  export default value;
+}
+declare module "*.jpg" {
+  const value: any;
+  export default value;
+}
+declare module "*.jpeg" {
+  const value: any;
+  export default value;
+}
+declare module "*.gif" {
+  const value: any;
+  export default value;
+}
+declare module "*.svg" {
+  const value: any;
+  export default value;
+}
+declare module "*.webp" {
+  const value: any;
+  export default value;
+}
+declare module "*.mp4" {
+  const value: any;
+  export default value;
+}
+declare module "*.webm" {
+  const value: any;
+  export default value;
+}
+declare module "*.ogg" {
+  const value: any;
+  export default value;
+}
